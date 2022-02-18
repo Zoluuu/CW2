cef0486ed648d1fed909a5e491a7d4f4526e8164
zol <izenonx@gmail.com>
2022-02-18 23:30:32 +0400

    added initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// Import dependencies modules:
+const express = require('express')
+const cors = require('cors');
+let dbCollection = null
+// const bodyParser = require('body-parser')
+
+// Create an Express.js instance:
+const app = express()
+
+// config Express.js
