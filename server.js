4684ade34f47e2b0a1993eeff36cc943c2dc8470
zol <izenonx@gmail.com>
2022-02-18 23:31:43 +0400

    added config for express.js

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ let dbCollection = null
 const app = express()
 
 // config Express.js
+
+app.use(cors())
+app.use(express.json())
+app.set('port', 3000)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+// connect to MongoDB
+const MongoClient = require('mongodb').MongoClient;
